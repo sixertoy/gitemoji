ad7db5303fc794c21ecf6da9d5b321904935aeb9
sixertoy <sixertoy.github@gmail.com>
2019-09-01 14:35:11 +0200

    🚧 generate markdown table for contributing file

--- index.js
+++ index.js
@@ -3,18 +3,28 @@
 const fs = require('fs');
 const path = require('path');
 const emoji = require('emojilib');
+const tablemark = require('tablemark');
 
 const EMOJIS_JSON_MAP = require('./emojis.json');
 const HOOK_OUTPUT_FOLDER = path.join(__dirname, 'build');
 const HOOK_TEMPLATE_FILE = path.join(__dirname, 'prepare-commit-msg.tpl');
 
 const REPLACER_JOKER = '%%BASH_ARRAY%%';
+const CONTRIBUTING_FILENAME = 'CONTRIBUTING.md';
 
 function getSymbolByType(input) {
   const firstchar = input.charAt(0);
   const isalpha = /^[a-z]$/.test(firstchar);
   if (isalpha) return emoji.lib[input].char;
   return input;
+}
+
+function generateMarkdownTable(emojiMap) {
+  const table = emojiMap.map(function getEntryArraySymbol({ keyword, tag }) {
+    const symbol = getSymbolByType(tag);
+    return { keyword, tag, symbol };
+  });
+  return tablemark(table);
 }
 
 function generateBashArrayEntries(emojiMap) {
@@ -43,21 +53,34 @@ function replaceTemplateJoker(input, value, joker) {
   return outputContent;
 }
 
-function writeHookFile(content, inputFilename, outputFolder) {
+function getHookOutputFilename(inputFilename) {
+  const basename = path.basename(inputFilename);
+  const [filename] = basename.split('.');
+  return filename;
+}
+
+function writeOuputFile(content, outputFilename, outputFolder) {
   try {
     fs.mkdirSync(outputFolder, { recursive: true });
   } catch (err) {}
-  const basename = path.basename(inputFilename);
-  const [filename] = basename.split('.');
-  const outputFile = path.join(outputFolder, filename);
+  const outputFile = path.join(outputFolder, outputFilename);
   fs.writeFileSync(outputFile, content, { encoding: 'utf8' });
 }
 
-function run(emojiMap, inputFilename, outputFolder, joker) {
+function run(emojiMap, inputFilename, outputFolder, joker, readmeFilename) {
   const bashArray = generateBashArrayEntries(emojiMap);
   const templateContent = getTemplateContent(inputFilename);
+  const hookFilename = getHookOutputFilename(inputFilename);
   const outputContent = replaceTemplateJoker(templateContent, bashArray, joker);
-  writeHookFile(outputContent, inputFilename, outputFolder);
+  writeOuputFile(outputContent, hookFilename, outputFolder);
+  const markdownTable = generateMarkdownTable(emojiMap);
+  writeOuputFile(markdownTable, readmeFilename, outputFolder);
 }
 
-run(EMOJIS_JSON_MAP, HOOK_TEMPLATE_FILE, HOOK_OUTPUT_FOLDER, REPLACER_JOKER);
+run(
+  EMOJIS_JSON_MAP,
+  HOOK_TEMPLATE_FILE,
+  HOOK_OUTPUT_FOLDER,
+  REPLACER_JOKER,
+  CONTRIBUTING_FILENAME
+);
